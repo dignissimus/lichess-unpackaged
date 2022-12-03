0d0835e4412e00e285de4f2bb5cfc32895d5b741
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-12-03 00:19:41 +0000

    Reflect change from bzip2 to zstd for compression

--- game-lengths/collect-data.sh
+++ game-lengths/collect-data.sh
@@ -5,5 +5,5 @@ VARIANTS=(standard antichess atomic chess960 crazyhouse horde kingOfTheHill raci
 mkdir -p data
 for variant in ${VARIANTS[@]}; do
     echo "Reading game lengths for variant \`$variant\`"
-    curl -s "https://database.lichess.org/$variant/lichess_db_${variant}_rated_$YEAR-$MONTH.pgn.bz2" | bzip2 -d | perl parse_lengths.pl > "data/$variant.target"
+    curl -s "https://database.lichess.org/$variant/lichess_db_${variant}_rated_$YEAR-$MONTH.pgn.zst" | zstdcat | perl parse_lengths.pl > "data/$variant.target"
 done
--- winning-chances/collect-data.sh
+++ winning-chances/collect-data.sh
@@ -1,5 +1,5 @@
 GAMES=100000
-URL="https://database.lichess.org/standard/lichess_db_standard_rated_2022-09.pgn.bz2"
+URL="https://database.lichess.org/standard/lichess_db_standard_rated_2022-09.pgn.zst"
 
 mkdir -p data
-curl $URL | bzip2 -d | python process_data.py
+curl $URL | zstdcat | python process_data.py
